5fccaaaee4b7d69e4c1551a524e50775147a4ff6
unknown <khuranayash110.yk@gmail.com>
2019-01-29 23:00:05 +0530

    Oauth added

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// check dev or prod
+if(process.env.NODE_ENV === 'production')
+{
+    module.exports = require('./prod');
+}
+else
+{
+    module.exports = require('./dev');
+}
---
+++ services/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('../config/keys');
+var mongoose = require('mongoose');
+const User = mongoose.model('users');
+
+passport.serializeUser((user,done) => {
+    console.log('serialize');
+    done(null,user.id);
+});
+
+passport.deserializeUser((id,done) => {
+    console.log('Deserialize');
+   
+    User.findById(id)
+    .then((user) => {
+        done(null,user)
+    });
+});
+passport.use(new GoogleStrategy({
+    clientID : keys.googleClientID ,
+    clientSecret : keys.googleClientSecret,
+    callbackURL : '/auth/google/callback'
+} , (accessToken,refreshToken,profile,done) => {
+        
+    User.findOne({googleId:profile.id})
+        .then((existingUser)=>{
+            if(existingUser)
+            {
+                done(null,existingUser);
+            }
+            else
+            {
+                new User ({googleId:profile.id})
+                .save()
+                .then(user => {
+                    done(null,user);
+                });
+            }
+        });   
+    })
+);
